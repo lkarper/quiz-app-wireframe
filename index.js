3beb00b97198b9ffb8114c3e6c909fdb9ebe6ecf
Luther Karper <luther.karper@live.com>
2020-03-23 13:42:23 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+//How well do you know the Julio-Claudian Emperors?
+const questions = [
+    {
+    question: "Who among the Julio-Claudian emperors reigned the longest?",
+    options: ["Augustus", "Tiberius", "Claudius", "Nero"],
+    correctAnswer: "Augustus" 
+    },
+    {
+    question: "What was Caligula's real name?",
+    options: ["Marcus", "Titus", "Publius", "Gaius"],
+    correctAnswer: "Gaius" 
+    },
+    {
+    question: "Which emperor completed the conquest of Britain?",
+    options: ["Tiberius", "Nero", "Claudius", "Caligula"],
+    correctAnswer: "Claudius" 
+    },
+    {
+    question: "Which of the emperors below was descended from both the Julian and Claudian families?",
+    options: ["Augustus", "Claudius", "Tiberius", "Caligula"],
+    correctAnswer: "Caligula" 
+    },
+    {
+    question: "Who retired early to Rhodes, only to come out of retirement after just seven years?",
+    options: ["Augustus", "Tiberius", "Claudius", "Nero"],
+    correctAnswer: "Tiberius" 
+    },
+    {
+    question: "Which emperor competed in the Olympic games?",
+    options: ["Caligula", "Augustus", "Nero", "Claudius"],
+    correctAnswer: "Nero" 
+    },
+    {
+    question: "Which emperor invented and added letters to the Latin alphabet?",
+    options: ["Claudius", "Augustus", "Caligula", "Tiberius"],
+    correctAnswer: "Claudius" 
+    },
+    {
+    question: "Which emperor conspired to have his own mother killed?",
+    options: ["Caligula", "Claudius", "Tiberius", "Nero"],
+    correctAnswer: "Nero" 
+    },
+    {
+    question: "Who added Egypt to the Roman empire?",
+    options: ["Tiberius", "Augustus", "Caligula", "Nero"],
+    correctAnswer: "Augustus" 
+    },
+    {
+    question: "Which of the Julio-Claudian emperors was the oldest when he died?",
+    options: ["Tiberius", "Augustus", "Claudius", "Nero"],
+    correctAnswer: "Tiberius" 
+    }
+];
+
